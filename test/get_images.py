9fb796e685eb08ce889ac7996dd47dd583d1faf7
= <=>
2024-04-15 08:57:44 -0300

    Missão Realizada Com Sucesso

--- test/get_images.py
+++ test/get_images.py
@@ -1,22 +1,32 @@
 from glob import glob
-from os.path import isdir
+from os.path import isdir, basename, join
 
 # Substitua 'caminho/da/pasta' pelo caminho do diretório que você quer pesquisar
 caminho_da_pasta = '../animals'
 
-def dir_images(caminho_da_pasta:str = "../animals"):
+def dir_images(caminho_da_pasta:str = "../animals", recursive: bool = False):
     files = []
+    valid_ext = ["jpg", "png", "jpeg","JPG", "PNG","JPEG"]
 
-    # Use a função glob com '**' para pesquisar recursivamente
-    for arquivo in glob(caminho_da_pasta + '/**', recursive=True):
-        if not isdir(arquivo):
-            files.append(arquivo)
-            
+    
+    if recursive == True:
+        # Use a função glob com '**' para pesquisar recursivamente
+        for arquivo in glob(caminho_da_pasta + '/**', recursive=True):
+            if not isdir(arquivo):
+                if basename(arquivo).split('.')[-1] in valid_ext:
+                    files.append(arquivo)
+    
+    else:
+        for ext in valid_ext:
+            for arquivo in glob(join(caminho_da_pasta,f"*.{ext}")):
+                if basename(arquivo).split('.')[-1] in valid_ext:
+                    files.append(arquivo)
             
     #print(len(files))
 
     
     return files
+
 
 
 #files = dir_images(caminho_da_pasta)
